51696dac8a89946d8d056a2d38bcb0e2d584b70f
xuamyj <xuamyj@gmail.com>
2018-11-17 23:55:54 -0800

    finished tutorial. great tutorial imo

--- FlutterApp/Fire.js
+++ FlutterApp/Fire.js
@@ -1,4 +1,86 @@
-class Fire {}
+import firebase from 'firebase';
+
+class Fire {
+  constructor() {
+    this.init();
+
+    this.observeAuth();
+  }
+
+  init = () =>
+    firebase.initializeApp({
+      apiKey: "AIzaSyCyWQi7euej7M1fH7GIBZWKhZcaUD0gpRM",
+      authDomain: "flutter-app-e2bd9.firebaseapp.com",
+      databaseURL: "https://flutter-app-e2bd9.firebaseio.com",
+      projectId: "flutter-app-e2bd9",
+      storageBucket: "flutter-app-e2bd9.appspot.com",
+      messagingSenderId: "725969630373"
+    });
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = user => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref('messages');
+  }
+
+  on = callback =>
+    this.ref
+      .limitToLast(20)
+      .on('child_added', snapshot => callback(this.parse(snapshot)));
+
+  parse = snapshot => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+
+    const timestamp = new Date(numberStamp);
+
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user,
+    };
+    return message;
+  }
+
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp,
+      };
+      this.append(message);
+    }
+  };
+
+  append = message => this.ref.push(message);
+
+  off() {
+    this.ref.off();
+  }
+}
 
 Fire.shared = new Fire();
 export default Fire;
--- FlutterApp/components/Chat.js
+++ FlutterApp/components/Chat.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 
+import Fire from '../Fire';
+
 class Chat extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: (navigation.state.params || {}).name || 'Chat!',
@@ -10,12 +12,34 @@ class Chat extends Component {
     messages: [],
   };
 
+  get user() {
+    // Return our name and our UID for GiftedChat to parse
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
   render() {
     return (
       <GiftedChat
         messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
       />
     );
+  }
+
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+
+  componentWillUnmount() {
+    Fire.shared.off();
   }
 }
 
