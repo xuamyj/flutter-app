23aee3a9d698008b310f27f961b93cb566c3cdc8
xuamyj <xuamyj@gmail.com>
2018-11-17 22:32:09 -0800

    finished 'Time to Code' section

--- FlutterApp/components/Main.js
+++ FlutterApp/components/Main.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+} from 'react-native';
 
 class Main extends Component {
+  state = { name: '' }
+
+  onPress = () => {
+    this.props.navigation.navigate('Chat', { name: this.state.name });
+  }
+
+  onChangeText = name => this.setState({ name });
+
   render() {
-    return <View />;
+    return (
+      <View>
+      <Text style={styles.title}>Enter your name:</Text>
+      <TextInput
+          onChangeText={this.onChangeText}
+          style={styles.nameInput}
+          placeHolder="Higgs Boson"
+          value={this.state.name}
+        />
+
+      <TouchableOpacity onPress={this.onPress}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+
+const offset = 24;
+const styles = StyleSheet.create({
+  nameInput: {
+    height: offset * 2,
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: offset,
+  },
+});
 
 export default Main;
