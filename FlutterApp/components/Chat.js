1fcec9b1bf62eda78eeff6eb6bedb81d1e3f64bd
xuamyj <xuamyj@gmail.com>
2018-11-17 22:50:47 -0800

    finished 'Chat Screen' section

--- FlutterApp/components/Chat.js
+++ FlutterApp/components/Chat.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 class Chat extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || 'Chat!',
+  });
+
+  state = {
+    messages: [],
+  };
+
   render() {
-    return <View />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+      />
+    );
   }
 }
-
-const styles = StyleSheet.create({});
 
 export default Chat;
